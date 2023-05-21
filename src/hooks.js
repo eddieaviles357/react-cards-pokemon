36a42e3abbc34fd30a82b42e5ebc89d2fb604e21
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-21 09:14:41 -0700

    useAxios hook added that takes a URL and makes a call to an api using axios

--- src/hooks.js
+++ src/hooks.js
@@ -1,9 +1,27 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
+import axios from 'axios';
+import {v4 as uuidv4} from 'uuid';
 
 const useFlip = () => {
     const [isFlipped, setIsFlipped] = useState(true);
-    const flip = () => setIsFlipped(flipped => !flipped)
+    const flip = () => setIsFlipped(flipped => !flipped);
     return [isFlipped, flip];
 }
 
-export {useFlip};
+const useAxios = (url) => {
+    if(!url) throw new Error('Please Enter a URL');
+    
+    const [axiosData, setAxiosData] = useState([]);
+
+    const setData = async() => {
+        try {
+            const {data} = await axios.get(url);
+            setAxiosData( axiosData => [...axiosData, { ...data, id: uuidv4() }] );
+        } catch (err) {
+            console.error(err);
+            return new Error(err.message);
+        }
+    }
+    return [axiosData, setData];
+}
+export {useFlip, useAxios};
