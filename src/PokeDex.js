81f153567a0e2ccd944b881cf6dcaab0262f76f9
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-21 12:09:35 -0700

    add pokemon function removed and was added to hooks logic, function now accepts 2 arguments

--- src/PokeDex.js
+++ src/PokeDex.js
@@ -1,28 +1,22 @@
-import React, { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
-import axios from "axios";
+import React from "react";
 import PokemonSelect from "./PokemonSelect";
 import PokemonCard from "./PokemonCard";
+import {useAxios} from './hooks';
 import "./PokeDex.css";
 
 /* Renders a list of pokemon cards.
  * Can also add a new card at random,
  * or from a dropdown of available pokemon. */
-function PokeDex() {
-  const [pokemon, setPokemon] = useState([]);
+const URL = 'https://pokeapi.co/api/v2/pokemon/';
 
-  const addPokemon = async name => {
-    const response = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${name}/`
-    );
-    setPokemon(pokemon => [...pokemon, { ...response.data, id: uuidv4() }]);
-  };
+function PokeDex() {
+  const [pokemon, setPokemon] = useAxios(URL);
   
   return (
     <div className="PokeDex">
       <div className="PokeDex-buttons">
         <h3>Please select your pokemon:</h3>
-        <PokemonSelect add={addPokemon} />
+        <PokemonSelect add={setPokemon} />
       </div>
       <div className="PokeDex-card-area">
         {pokemon.map(cardData => (
